84ee18220e71969a1fb137124ed01f853500129a
Paul Jenkins <paul@turnpiece.com>
2025-10-22 12:06:08 +0100

    Fix the start script

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 # 1. Activate virtual environment
 source venv/bin/activate
 
-# 2. Run the development server
-uvicorn main:app --reload
+# 2. Run the development server in background
+uvicorn main:app --reload &
 
 # 3. Run the worker
 python job_worker.py
